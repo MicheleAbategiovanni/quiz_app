aafc34a87499fda6ea066827fba25efaaf0d79ca
Michele Abategiovanni <112349713+MicheleAbategiovanni@users.noreply.github.com>
2023-07-27 00:07:09 +0200

    Add widget for answer button

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -12,6 +13,34 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello World');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Question',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(
+            title: 'Answer 1',
+            onTap: () {},
+          ),
+          AnswerButton(
+            title: 'Answer 2',
+            onTap: () {},
+          ),
+          AnswerButton(
+            title: 'Answer 3',
+            onTap: () {},
+          ),
+          AnswerButton(
+            title: 'Answer 4',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
